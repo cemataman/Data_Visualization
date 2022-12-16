a215037f629700c48a0e69792efc5f42c0cc6790
cemataman <cemataman93@gmail.com>
2022-12-16 17:16:00 +0800

    data preparation for the related diagram

--- Data_Preparation_1.py
+++ Data_Preparation_1.py
@@ -1,13 +1,5 @@
 import pandas as pd
-df = pd.read_excel("/Users/cem_ataman/Desktop/conceptioncomments.xlsx")
-
-'''
-df["Victim's age"] = pd.to_numeric(df["Victim's age"], errors='coerce').fillna(0).astype(np.int64)
-df.rename(columns={'Fleeing (Source: WaPo)': 'Fleeing'}, inplace=True)
-
-df = df[df["State"].isin(['NY', 'CA', 'TX'])]
-df = df[df["Victim's race"].isin(["White", "Black", "Hispanic", "Asian"])]
-'''
+df = pd.read_excel("//Users/cem_ataman/Dropbox/Research/Collaborations/Hamburg Group/Participation Data/Drupal 8 (2020-21)/41. Magistrale Wandsbek/conceptioncomments.xlsx")
 
 def check_if_cell(row):
     return row and type(row) == str
@@ -15,19 +7,17 @@ def check_if_cell(row):
 # creating a dictionary with an empty list to store main arguments
 cdict = {"main": []}
 
-#for k in range(len(df["Contribution ID"]))
-
 for j in range(len(df["Comment ID"])):
     cid = df["Comment ID"][j]
     if not check_if_cell(df["Comment Subject"][j]):
-        cdict["main"].append(cid) #, df["created (UTC)"][j])
+        cdict["main"].append((cid, df["created (UTC)"][j], df["Comment Text"][j]))
     cdict[cid] = []
     for i in range(len(df["Comment Subject"])):
         subject = df["Comment Subject"][i]
         if check_if_cell(subject):
             sid = subject.split(" ")[-1]
             if int(cid) == int(sid):
-                cdict[cid].append(df["Comment ID"][i]) # df["created (UTC)"][i])
+                cdict[cid].append((df["Comment ID"][i], df["created (UTC)"][i], df["Comment Text"][i]))
 
 cdict = {k: v for k, v in cdict.items() if v}
 print(cdict)
